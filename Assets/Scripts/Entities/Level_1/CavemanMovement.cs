2c20abbf5ff815a0f5c1677642bbaa80e0c640ab
Andrés Rodríguez <andresteban2002@gmail.com>
2023-03-03 22:03:16 -0500

    Muerte por caida

--- Assets/Scripts/Entities/Level_1/CavemanMovement.cs
+++ Assets/Scripts/Entities/Level_1/CavemanMovement.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 
 public class CavemanMovement : MonoBehaviour
 {
+    public GameObject player;
+
+    private SpriteRenderer _spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 180, 0);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player)
+        {
+            if (player.transform.position.x > transform.position.x)
+            {
+                _spriteRenderer.flipX = false;
+            }
+            else
+            {
+                _spriteRenderer.flipX = true;
+            }
+        }
     }
 
     private void FixedUpdate()
