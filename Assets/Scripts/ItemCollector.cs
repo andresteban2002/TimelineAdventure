54bb95032042f493f7223eabd7b246d882d19325
EnriRed <86379782+EnriRed@users.noreply.github.com>
2023-03-22 18:15:04 -0500

    Ajustes para guardado de la partida. En desarrollo

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
     private int stones = 0;
     [SerializeField] private Text stonesText;
+    [SerializeField] float minX, maxX, minY, maxY;
+    [SerializeField] GameObject posXTxt, posYTxt;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,6 +20,18 @@ public class ItemCollector : MonoBehaviour
             stones++;
             stonesText.text = stones.ToString();
         }
+    }
+
+    public Vector2 GetPosition(){
+        return transform.position;
+    }
+
+    public int GetStones(){
+        return stones;
+    }
+
+    public void SetPosition(Vector2 pos){
+        transform.position = pos;
     }
 
     
