cb24da219efd95c73f350b72297ae9888c60fe7c
Andrés Rodríguez <andresteban2002@gmail.com>
2023-03-24 16:36:26 -0500

    save button en menu de pausa

--- Assets/Scripts/Map/ItemCollector.cs
+++ Assets/Scripts/Map/ItemCollector.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
     private int stones = 0;
     [SerializeField] private Text stonesText;
+    [SerializeField] float minX, maxX, minY, maxY;
+    [SerializeField] GameObject posXTxt, posYTxt;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,5 +22,15 @@ public class ItemCollector : MonoBehaviour
         }
     }
 
-    
+    public Vector2 GetPosition(){
+        return transform.position;
+    }
+
+    public int GetStones(){
+        return stones;
+    }
+
+    public void SetPosition(Vector2 pos){
+        transform.position = pos;
+    }
 }
