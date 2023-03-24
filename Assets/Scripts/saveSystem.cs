20304f4abee357174d26c4fdaff853ba6ce85ad8
Andrés Rodríguez <andresteban2002@gmail.com>
2023-03-24 15:12:53 -0500

    saveSystem Script

---
+++ Assets/Scripts/saveSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+
+public class saveSystem : MonoBehaviour
+{
+    ItemCollector playerScr;
+    private void Awake(){
+        playerScr = FindObjectOfType<ItemCollector>();
+    }
+
+    public void SaveGame(){
+        Vector2 playerPos = playerScr.GetPosition();
+        int stones = playerScr.GetStones();
+
+        PlayerPrefs.SetFloat("posX", playerPos.x);
+        PlayerPrefs.SetFloat("posY", playerPos.y);
+        Debug.Log(">>>>>>>>>>>>>>>" + stones.ToString());
+    }
+    public void LoadGame(){
+        Vector2 playerPos = new Vector2(PlayerPrefs.GetFloat("posX"), PlayerPrefs.GetFloat("posY"));
+
+        playerScr.SetPosition(playerPos);
+    } 
+}
