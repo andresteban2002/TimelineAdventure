5a9b57a303a58c3410ccda455f96c2b6d9c5382c
Karen Vanesa Cely Gutierrez <kcely@poligran.edu.co>
2023-03-26 19:52:10 -0500

    Script movimiento Cavewoman_walk

---
+++ Assets/Scripts/Entities/Level_1/CavewomanMovement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+
+public class CavewomanMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    private SpriteRenderer _spriteRenderer;
+    private const string CAVEWOMAN_WALK = "Cavewoman_Walk";
+    private Animator _animator;
+    private string currentStep;
+
+    private string direction = "right";
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        changeAnimationState(CAVEWOMAN_WALK);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == "right")
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 4.0f);
+        }
+        else if (direction == "left")
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 4.0f);
+        }
+    }
+    
+    public void changeAnimationState(string newState)
+    {
+        if(currentStep==newState) return;
+        _animator.Play(newState);
+        currentStep = newState;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("enemyLimit"))
+        {
+            if (direction == "right")
+            {
+                _spriteRenderer.flipX = true;
+                direction = "left";
+            }
+            else if (direction == "left")
+            {
+                _spriteRenderer.flipX = false;
+                direction = "right";
+            }
+        }
+    }
+}
